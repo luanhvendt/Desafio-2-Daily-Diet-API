5ce7119f430b17268f2c93ecb3fd0957225dad98
Luan Vendt <luan.vendt@bitize.com.br>
2024-01-11 10:55:34 -0300

    adicionando tabela User

---
+++ prisma/migrations/20240111135407_add_user/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "userId" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
