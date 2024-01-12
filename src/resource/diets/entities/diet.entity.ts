1fa6a11a0ac3b6a6e9b293dd1319760fc28f49b7
Luan Vendt <luan.vendt@bitize.com.br>
2024-01-12 16:41:44 -0300

    alterando tipo para interface de diet e user entitys

--- src/resource/diets/entities/diet.entity.ts
+++ src/resource/diets/entities/diet.entity.ts
@@ -1,4 +1,4 @@
-export type DietEntity = {
+export interface DietEntity  {
     id?: string;
     name: string;
     description: string;
--- src/resource/users/entities/user.entity.ts
+++ src/resource/users/entities/user.entity.ts
@@ -1,4 +1,4 @@
-export type UserEntity = {
+export interface UserEntity {
     id?: string;
     name: string;
     email: string;
