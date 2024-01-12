b69adbe232c3b7b61d2bb07f76bcfe89b5a26dce
Luan Vendt <luan.vendt@bitize.com.br>
2024-01-12 10:12:30 -0300

    InMemoryUsersReósitory

--- src/resource/users/entities/user.entity.ts
+++ src/resource/users/entities/user.entity.ts
@@ -3,4 +3,5 @@ export type UserEntity = {
     name: string;
     email: string;
     password: string;
+    newPassword?: string;
 }
---
+++ test/repositories/InMemoryUsersRepository.ts
@@ -0,0 +1,52 @@
+import { UpdateUserDto } from "src/resource/users/dto/update-user.dto";
+import { UserEntity } from "src/resource/users/entities/user.entity";
+import { UsersRepository } from "src/resource/users/repositories/user.repository";
+
+export class InMemoryUsersRepository implements UsersRepository {
+    public items: any = []
+
+    async create(data: UserEntity) {
+        const user: UserEntity = {
+            id: '1',
+            name: data.name,
+            email: data.email,
+            password: data.password
+        }
+
+        this.items.push(user)
+        return user
+    }
+
+    async findAll() {
+        return this.items
+    }
+
+    async findUnique(id: string) {
+        for (let i = 0; i < this.items.length; i++) {
+            if (this.items[i].id === id) {
+                return this.items[i]
+            }
+        }
+    }
+
+    async update(id: string, dataUser: UpdateUserDto) {
+        for (let i = 0; i < this.items.length; i++) {
+            if (this.items[i].id === id) {
+                this.items[i].name = dataUser.name
+                this.items[i].email = dataUser.email
+                this.items[i].password = dataUser.password
+                this.items[i].newPassword = dataUser.newPassword
+
+                return this.items[i]
+            }
+        }
+    }
+
+    async delete(id: string) {
+        for (let i = 0; i < this.items.length; i++) {
+            if (this.items[i].id === id) {
+                this.items.splice(i, 1);
+            }
+        }
+    }
+}
